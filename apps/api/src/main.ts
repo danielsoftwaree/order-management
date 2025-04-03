6faed93305480b09908b452539b41d1eed79695a
Daniel <etseter11@gmail.com>
2025-04-03 16:10:20 +0200

    feat(api): added global interceptor for data transformation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ApiExceptionFilter } from './common/filters/api-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     app.useGlobalFilters(new ApiExceptionFilter());
+
+    app.useGlobalInterceptors(new TransformInterceptor());
 
     app.useGlobalPipes(
         new ValidationPipe({
