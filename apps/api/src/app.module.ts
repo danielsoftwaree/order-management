d2f035b3ff255af8a33068ba75e5ef9238129a55
Daniel <etseter11@gmail.com>
2025-04-03 13:20:10 +0200

    feat(api): apply auth middleware to all routes

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthMiddleware } from './auth/middlewares/auth.middleware';
 
 @Module({
   imports: [ProductModule, UserModule, OrderModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  //
+  // apply auth middleware to all routes
+  // 
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
